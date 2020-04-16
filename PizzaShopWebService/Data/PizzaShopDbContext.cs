6da2fc857fae0695abb8a6e672fa5006e415a750
Surafel Assefa <surafel911@gmail.com>
2020-04-16 04:22:53 -0400

    Adds employee to DbContext.

--- PizzaShopWebService/Data/PizzaShopDbContext.cs
+++ PizzaShopWebService/Data/PizzaShopDbContext.cs
@@ -4,24 +4,31 @@ using PizzaShopWebService.Models;
 
 namespace PizzaShopWebService.Data
 {
-    public class PizzaShopDbContext : DbContext
-    {
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Transaction> Transactions { get; set; }
-        
-        public PizzaShopDbContext (DbContextOptions<PizzaShopDbContext> options)
-            : base(options)
-        {
-        }
+	public class PizzaShopDbContext : DbContext
+	{
+		public DbSet<Manager> Managers { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Customer>()
-                .HasKey(customer => customer.PhoneNumber);
-            modelBuilder.Entity<Transaction>()
-                .HasKey(transaction => transaction.TransactionID);
-            modelBuilder.Entity<Manager>()
-                .HasKey(manager => manager.PhoneNumber);
-        }
-    }
+		public DbSet<Employee> Employees { get; set; }
+
+		public DbSet<Customer> Customers { get; set; }
+
+		public DbSet<Transaction> Transactions { get; set; }
+		
+		public PizzaShopDbContext (DbContextOptions<PizzaShopDbContext> options)
+			: base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Manager>()
+				.HasKey(manager => manager.PhoneNumber);
+            modelBuilder.Entity<Employee>()
+                .HasKey(employee => employee.PhoneNumber);				
+			modelBuilder.Entity<Customer>()
+				.HasKey(customer => customer.PhoneNumber);
+			modelBuilder.Entity<Transaction>()
+				.HasKey(transaction => transaction.TransactionID);
+		}
+	}
 }
