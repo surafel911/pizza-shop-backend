2fde2bc7ed7250e285b1d9cd1beb20d7abdc8293
Surafel Assefa <surafel911@gmail.com>
2020-04-23 20:18:30 -0400

    Adds/renames new payment pages.

---
+++ PizzaShopWebService/Pages/PaymentMethod.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+
+using PizzaShopWebService.Models;
+using PizzaShopWebService.Services;
+
+namespace PizzaShopWebService.Pages
+{
+    public class PaymentMethodModel : PageModel
+    {
+        private readonly IPizzaShopDbHandler _pizzaShopDbHandler;
+
+        [BindProperty]
+        public PaymentType PaymentType { get; set; }
+
+        public PaymentMethodModel(IPizzaShopDbHandler pizzaShopDbHandler)
+        {
+            _pizzaShopDbHandler = pizzaShopDbHandler;
+        }
+
+        public IActionResult OnGet()
+        {
+            string phoneNumber;
+            Customer customer;
+
+            phoneNumber = HttpContext.Session.GetString("PhoneNumber");
+            if (string.IsNullOrEmpty(phoneNumber)) {
+                // TODO: Handle this condition better
+                return Content("Login required.");
+            }
+
+            customer = _pizzaShopDbHandler.FindCustomer(phoneNumber);
+            if (customer == null) {
+                // TODO: Handle this condition better
+                return Content("No customer account in this phone number.");
+            }
+
+            PaymentType = customer.PaymentType;
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            HttpContext.Session.SetInt32("PaymentType", (int)PaymentType);
+
+            if (PaymentType == PaymentType.Cash || 
+                PaymentType == PaymentType.Check) {
+                return RedirectToPage("/Confirmation");
+            }
+            
+            return RedirectToPage("/Payment");
+        }
+    }
+}
